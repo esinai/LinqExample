3547530f2ddab1947384586fe93003fec4faa9f5
Erez Sinai <erez.sinai@gmail.com>
2023-11-20 11:10:42 +0200

    Add project files.

---
+++ mainWin.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace LinqExample
+{
+    partial class mainWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.AllowUserToAddRows = false;
+            this.dgvShow.AllowUserToDeleteRows = false;
+            this.dgvShow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dgvShow.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Location = new System.Drawing.Point(21, 134);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.ReadOnly = true;
+            this.dgvShow.RowHeadersVisible = false;
+            this.dgvShow.Size = new System.Drawing.Size(737, 295);
+            this.dgvShow.TabIndex = 0;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(47, 28);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Run";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(790, 160);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.Text = "Prev";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(792, 235);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1535, 367);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // mainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.dgvShow);
+            this.Name = "mainWin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainWin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ mainWin.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqExample
+{
+    public partial class mainWin : Form
+    {
+        dbEntities db = new dbEntities();
+        List<UserTBL> userList;
+        List<ConnectionsTBL> conList;
+        int pos = 0;
+        int pageSize= 123;
+        public mainWin()
+        {
+            InitializeComponent();
+        }
+       
+        private void mainWin_Load(object sender, EventArgs e)
+        {
+            userList = (from s in db.UserTBL select s).ToList();
+            conList = (from s in db.ConnectionsTBL select s).ToList();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            var tmp = (from s in userList
+                       join d in conList
+                       on s.id equals d.userID
+                       select new
+                       {
+                           userID = s.id,
+                           FullName = s.first_name + ' ' + s.last_name,
+                           s.gender,
+                           d.loginDateTime,
+                           d.logoutDateTime
+                       }).ToList();
+
+            var result = tmp.GroupBy(x => new { x.loginDateTime.Value.Year , x.loginDateTime.Value.Month}).ToList();
+            List<yearMonthView> yList = new List<yearMonthView>();
+            foreach (var item in result)
+            {
+                yearMonthView y = new yearMonthView();
+                y.year = item.Key.Year;
+                y.month = item.Key.Month;
+                y.count = item.Count();
+                yList.Add(y);
+            }
+            DateTime t1 = DateTime.Now;
+            DateTime t2 = DateTime.Now.AddMinutes(-17);
+            TimeSpan ts = t1 - t2;
+            yList = yList.OrderByDescending(x => x.year).ThenByDescending(x => x.month).ToList();
+            dgvShow.DataSource = yList;
+        }
+
+        public void showResult()
+        {
+            var tmp = userList.Skip(pos).Take(pageSize).ToList();
+
+            dgvShow.DataSource = tmp;
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (pos - pageSize < 0)
+                return;
+            pos = pos - pageSize;
+            showResult();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (userList.Count - pos < pageSize)
+                return;
+            pos += pageSize;
+            showResult();
+        }
+
+        public class yearMonthView
+        {
+            public int year { get; set; }
+            public int month { get; set; }
+            public int count { get; set; }
+        }
+
+        
+    }
+}
